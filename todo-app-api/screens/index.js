f2a20aac4eadac1732710bef928b74b2c401b8e7
ELANGSPB <115124715+ESPB0808@users.noreply.github.com>
2023-12-01 11:00:07 +0700

    Delete todo-app-api directory

--- todo-app-api/screens/index.js
+++
@@ -1,6 +0,0 @@
-import AboutScreen from "./about";
-import TaskScreen from "./task_all";
-import TaskCompletedScreen from "./task_completed";
-import ProfileScreen from "./profile";
-
-export { AboutScreen, TaskCompletedScreen, TaskScreen, ProfileScreen };
